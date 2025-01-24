93847653ffe9b16ad223f4cba873220b012d2259
LongNguyen <146229690+LongNguyenC0der@users.noreply.github.com>
2025-01-23 23:45:44 -0500

    Arrive added, might need some adjustment

--- Assets/Scripts/PlaySceneGameMode.cs
+++ Assets/Scripts/PlaySceneGameMode.cs
@@ -12,14 +12,24 @@ public enum EPlayMode : byte
 public class PlaySceneGameMode : MonoBehaviour
 {
     private const float BOUNDARY = 7.0f;
+    private const float MOVE_SPEED = 3.0f;
+    private const float TURN_SPEED = 100.0f;
+    private const float RADIUS = 1.0f;
+
+
     [SerializeField] private GameObject playerPrefab;
     [SerializeField] private GameObject targetPrefab;
     [SerializeField] private GameObject enemyPrefab;
     private GameObject player;
     private GameObject target;
     private GameObject enemy;
-    private float moveSpeed = 3.0f;
-    private float turnSpeed = 100.0f;
+    
+    
+
+    // for arrive algorithm
+    
+    private float timeToTarget = 2.0f;
+
     private EPlayMode playMode;
 
     private void Start()
@@ -53,7 +63,8 @@ public class PlaySceneGameMode : MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Alpha3))
         {
-            Debug.Log("Arrive");
+            playMode = EPlayMode.LinearArrive;
+            SetUpActors(playMode);
         }
         else if (Input.GetKeyDown(KeyCode.Keypad4) || Input.GetKeyDown(KeyCode.Alpha4))
         {
@@ -71,6 +82,7 @@ public class PlaySceneGameMode : MonoBehaviour
                 LinearFlee();
                 break;
             case EPlayMode.LinearArrive:
+                LinearArrive();
                 break;
             case EPlayMode.LinearAvoid:
                 break;
@@ -90,6 +102,7 @@ public class PlaySceneGameMode : MonoBehaviour
             case EPlayMode.Standby:
                 break;
             case EPlayMode.LinearSeek:
+            case EPlayMode.LinearArrive:
                 player.transform.position = GetRandomPosition();
                 target.transform.position = GetRandomPosition();
                 player.SetActive(true);
@@ -100,8 +113,6 @@ public class PlaySceneGameMode : MonoBehaviour
                 enemy.transform.position = GetRandomPosition();
                 player.SetActive(true);
                 enemy.SetActive(true);
-                break;
-            case EPlayMode.LinearArrive:
                 break;
             case EPlayMode.LinearAvoid:
                 break;
@@ -118,7 +129,7 @@ public class PlaySceneGameMode : MonoBehaviour
     private void LinearSeek()
     {
         Vector3 direction = (target.transform.position - player.transform.position).normalized;
-        Vector3 distanceToMove = direction * moveSpeed * Time.deltaTime;
+        Vector3 distanceToMove = direction * MOVE_SPEED * Time.deltaTime;
         player.transform.position += distanceToMove;
         Turning(direction);
     }
@@ -126,7 +137,7 @@ public class PlaySceneGameMode : MonoBehaviour
     private void LinearFlee()
     {
         Vector3 direction = (player.transform.position - enemy.transform.position).normalized;
-        Vector3 distanceToMove = direction * moveSpeed * Time.deltaTime;
+        Vector3 distanceToMove = direction * MOVE_SPEED * Time.deltaTime;
         Vector3 newPosition = player.transform.position + distanceToMove;
 
         float x = newPosition.x;
@@ -144,11 +155,28 @@ public class PlaySceneGameMode : MonoBehaviour
         Turning(direction);
     }
 
+    private void LinearArrive()
+    {
+        Vector3 distance = target.transform.position - player.transform.position;
+        if (distance.magnitude > RADIUS)
+        {
+            distance /= timeToTarget;
+            if (distance.magnitude > MOVE_SPEED)
+            {
+                distance = distance.normalized * MOVE_SPEED;
+            }
+            //Vector3 direction = distance.normalized;
+            Vector3 distanceToMove = distance * Time.deltaTime;
+            player.transform.position += distanceToMove;
+            Turning(distance);
+        }
+    }
+
     private void Turning(Vector3 direction)
     {
         float currentAngle = player.transform.eulerAngles.y;
         float desiredAngle = Vector3.SignedAngle(player.transform.forward, direction, Vector3.up); // Return relative angle
-        float newAngle = Mathf.MoveTowardsAngle(currentAngle, currentAngle + desiredAngle, turnSpeed * Time.deltaTime); // 2 absolute angles
+        float newAngle = Mathf.MoveTowardsAngle(currentAngle, currentAngle + desiredAngle, TURN_SPEED * Time.deltaTime); // 2 absolute angles
         player.transform.rotation = Quaternion.Euler(0.0f, newAngle, 0.0f);
     }
 }
